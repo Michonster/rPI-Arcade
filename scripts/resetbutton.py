4a7172edb4998ad33fd433b895f5cd8f757940da
Jameson Giannattasio <jamesongee08@gmail.com>
2024-11-05 00:34:07 -0500

    Update resetbutton.py

--- scripts/resetbutton.py
+++ scripts/resetbutton.py
@@ -1,20 +1,22 @@
-"""Code successfully rebooted and turned off the pi but was unable to
-turn the pi on from a shutdown state"""
+""" Program meant to be run on startup, allowing for the user 
+to press a button and trigger a reset (reboot) of the raspberry pi. 
+Instructions on how to run on bootup can be found in the rPIArcade 
+README. """
 
 from gpiozero import Button
 import time
 import subprocess
 import os
 from signal import pause
-print("waiting for reset to be pressed")
-# Setup button on pin 5 (BCM numbering)
+
+#Utilizes GPIO Pin 17
 button = Button(17)
 
+#when reset is pressed, reboot the raspberry pi
 def handle_button_press():
-    press_time = time.time()
     if button.is_pressed:
         print("Now rebooting RPi")
-        time.sleep(5)
+        time.sleep(1)
         os.system("sudo reboot")
 
 try:
@@ -24,28 +26,3 @@ try:
 except KeyboardInterrupt:
     print("User Ended")
 
-"""import RPi.GPIO as GPIO
-import time
-import subprocess
-
-# we will use the pin numbering to match the pins on the Pi, instead of the 
-# GPIO pin outs (makes it easier to keep track of things)
-# Pin 5 = GPIO 3
-GPIO.setmode(GPIO.BOARD)  
-
-# use the same pin that is used for the reset button
-GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
-
-oldButtonState1 = True
-
-while True:
-    #grab the current button state
-    buttonState1 = GPIO.input(5)
-
-    # check to see if button has been pushed
-    if buttonState1 != oldButtonState1 and buttonState1 == False:
-      subprocess.call("shutdown -h now", shell=True, 
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-      oldButtonState1 = buttonState1
-
-    time.sleep(.1)"""
