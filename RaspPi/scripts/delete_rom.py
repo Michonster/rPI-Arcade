556e307f1b59a5356addf08b0c342a2e3660f984
Jameson Giannattasio <jamesongee08@gmail.com>
2024-11-13 17:54:12 -0500

    create deletion script

---
+++ RaspPi/scripts/delete_rom.py
@@ -0,0 +1,62 @@
+import subprocess
+import os
+
+deleting = True
+game_systems = ['nes', 'n64', 'snes', 'dreamcast', 'psp', 'psx', 'nds', 'megadrive']
+
+while True:
+    print("\n0 :  Exit deletion")
+    for i in range(len(game_systems)):
+        print(i+1, ": ", game_systems[i])
+    emu = int(input("Select which emulator you wish to delete games from: "))
+    if emu == 0:
+        break
+    elif emu>len(game_systems) or emu<0:
+        continue
+    
+    system = game_systems[emu-1]
+    path = os.path.join("/home/rpiarcade/RetroPie/roms/", system)
+    inemu = True
+    while inemu:
+        games = []
+        for game in os.listdir(path):
+            if game.endswith(".zip") or game.endswith(".7z"):
+                base_game_name = game.split('(')[0].rsplit('.', 1)[0]
+                games.append(game)
+                if len(games) ==1:
+                    print("\n0 :  Back to Emulator Screen")
+                print(len(games), ": ", base_game_name)
+        
+        if not games:  # Check if there are no games
+            print("No games found in this directory.\n")
+            inemu=False
+            break 
+
+        print(len(games)+1, " :  Clear All")
+        
+        rom = int(input("Select which rom you wish to delete: "))
+        if rom == 0:  
+            print("Returning to Emulator Screen")
+            inemu=False
+            break
+        elif 1 <= rom <= len(games):  # Valid game selection
+            game_to_delete = games[rom - 1]
+            game_path = os.path.join(path, game_to_delete)
+            os.remove(game_path)
+            base_game_name = game_to_delete.split('(')[0].rsplit('.', 1)[0]
+            for file in os.listdir(path):
+                if file.startswith(base_game_name):
+                    file_path = os.path.join(path, file)
+                    os.remove(file_path)
+            print(f"{base_game_name} has been deleted.")
+
+        elif rom == len(games):
+            print(f"\nDeleting all games in {system}...")
+            for game in games:
+                game_path = os.path.join(path, game)
+                os.remove(game_path)
+            print("All games deleted.")
+            inemu=False
+            break
+        else:
+            print("Invalid selection")
