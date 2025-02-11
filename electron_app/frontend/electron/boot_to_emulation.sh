4f084893133484220d66b1c808164f651c26c6ab
Leann Quinlan <lequin1@woh.rr.com>
2025-02-11 17:31:13 -0500

    Added proper commands to the bash scripts

--- electron_app/frontend/electron/boot_to_emulation.sh
+++ electron_app/frontend/electron/boot_to_emulation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Hello, world!"
+
+# Switch to tty1 (CTRL + ALT + F1)
+chvt 1
+
+# Wait for a moment to ensure the switch happens
+sleep 1
+
+# Type the command to start emulationstation
+echo "emulationstation" | sudo -u YOUR_USER_NAME bash -c "cat > /dev/tty1"
