517b4d0c1c0e1c8f9e15fea755dd6b6b5ed3a6b6
Leann Quinlan <lequin1@woh.rr.com>
2025-03-18 17:07:07 -0400

    Trying to add loading screen to show when games are loading

--- electron_app/backend/boot_to_emulation.sh
+++ electron_app/backend/boot_to_emulation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Show a splash screen (image)
+feh --fullscreen --hide-pointer --quiet /home/rpiarcade/electron_app/backend/load.jpg &
+
 # Switch to tty1 (CTRL + ALT + F1)
 chvt 1
 
@@ -8,3 +11,6 @@ export DISPLAY=:0
 
 # Run EmulationStation with the correct environment
 sudo -u rpiarcade bash -c 'source /home/rpiarcade/.profile; emulationstation' > /home/rpiarcade/emulationstation_log.txt 2>&1
+
+# Kill the splash screen after EmulationStation starts
+kill %1
