502125b14de94fe3daa69c347f280e5836e1d6b0
Leann Quinlan <lequin1@woh.rr.com>
2025-03-18 17:13:54 -0400

    Trying to make the loading screen visible after application is closed

--- electron_app/backend/boot_to_emulation.sh
+++ electron_app/backend/boot_to_emulation.sh
@@ -3,6 +3,9 @@
 # Show a splash screen (image)
 feh --fullscreen --hide-pointer --quiet /home/rpiarcade/electron_app/backend/load.jpg &
 
+# Save the splash screen process ID to kill it later
+LOAD_PID=$!
+
 # Switch to tty1 (CTRL + ALT + F1)
 chvt 1
 
@@ -12,5 +15,6 @@ export DISPLAY=:0
 # Run EmulationStation with the correct environment
 sudo -u rpiarcade bash -c 'source /home/rpiarcade/.profile; emulationstation' > /home/rpiarcade/emulationstation_log.txt 2>&1
 
-# Kill the splash screen after EmulationStation starts
-kill %1
+# Wait for a bit to keep the splash visible, then kill the splash screen
+sleep 5
+kill $LOAD_PID
